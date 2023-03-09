13a74d5af3c46e57e80ece5ae3d8b7393aa036b7
Bardia Khosravi <bardiakhosraviesmaeili@gmail.com>
2023-03-09 22:58:46 +0330

    adding tags and filters

--- home/views.py
+++ home/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 
 
 def say_hello(request):
-    return render(request, 'hello.html')
-
+    return render(request, 'hello.html', context={'name': 'Bardia', 'last_name': 'khosravi esmaeili'})
 
 def home(request):
     return render(request, 'home.html')
