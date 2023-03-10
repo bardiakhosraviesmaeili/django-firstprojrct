193436e947a30ea743221038101f920f84b6e520
Bardia Khosravi <bardiakhosraviesmaeili@gmail.com>
2023-03-10 11:46:26 +0330

    making detail pages

--- home/views.py
+++ home/views.py
@@ -7,5 +7,10 @@ def say_hello(request):
 
 
 def home(request):
-    all=Todo.objects.all()
-    return render(request, 'home.html', {'todos':all})
+    all = Todo.objects.all()
+    return render(request, 'home.html', {'todos': all})
+
+
+def detail(request, todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    return render(request, 'detail.html', {'todo': todo})
