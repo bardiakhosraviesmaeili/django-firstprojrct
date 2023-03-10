43d808c6b4600189763da33e455cb05c3b790780
Bardia Khosravi <bardiakhosraviesmaeili@gmail.com>
2023-03-10 09:54:52 +0330

    working with sql and making a todo list

--- home/views.py
+++ home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Todo
 
 
 def say_hello(request):
     return render(request, 'hello.html', context={'name': 'Bardia', 'last_name': 'khosravi esmaeili'})
 
+
 def home(request):
-    return render(request, 'home.html')
+    all=Todo.objects.all()
+    return render(request, 'home.html', {'todos':all})
