6423b1b3368ea80743d7dd074896bcce4e4088a8
Bardia Khosravi <bardiakhosraviesmaeili@gmail.com>
2023-03-13 13:05:52 +0330

    adding the message

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('delete/ <int:todo_id>',views.delete, name= 'delete'),
+    path('delete/ <int:todo_id>', views.delete, name='delete'),
     path('detail/<int:todo_id>', views.detail, name='detail')
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Todo
+from django.contrib import messages
 
 
 def say_hello(request):
@@ -16,4 +17,5 @@ def detail(request, todo_id):
     return render(request, 'detail.html', {'todo': todo})
 def delete(request, todo_id):
     Todo.objects.get(id=todo_id).delete()
+    messages.success(request,"todo deleted successfuly","success")
     return redirect('home')
